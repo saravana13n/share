a721beabace84a42010c9de1488b38e9f916c1c7
saravana13n <saravana13n@gmail.com>
2018-03-22 19:33:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+function fbShare(){
+	window.plugins.socialsharing.shareViaFacebook('Message via Facebook', 
+                                                 null /* img */, 
+                                                 null /* url */, 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+};
+
+function whatsappShare(){
+	window.plugins.socialsharing.shareViaWhatsApp('Message via WhatsApp',
+                                                 null /* img */,
+                                                 "https://pointdeveloper.com/" /* url */, 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+};
+
+function twitterShare(){
+	 window.plugins.socialsharing.shareViaTwitter('Message via Twitter',
+                                                 null /* img */, 
+                                                 'http://twitter.com/', 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+  };
+
+ function otherShare(){
+	 window.plugins.socialsharing.share('General share Message', null, null, null);
+  };
+
+function onDeviceReady() {
+}
+
+
+function domLoaded(){
+  document.addEventListener("deviceready", onDeviceReady, false);
+
+}
